72b3747980b9a25b01222f321b148e5403049087
kmg31815 <kmg31815@gmail.com>
2019-06-24 17:31:09 +0800

    homework1-01

---
+++ SQLQuery1.sql
@@ -0,0 +1,11 @@
+select * from dbo.BOOK_LEND_RECORD;
+select * from dbo.MEMBER_M;
+select * from dbo.BOOK_CLASS;
+select * from dbo.BOOK_CODE;
+select * from dbo.BOOK_DATA;
+
+select blr.KEEPER_ID KeeperId, mm.USER_CNAME CName, mm.USER_ENAME EName, year(blr.LEND_DATE) BorrowYear, COUNT(*) BorrowCnt
+from MEMBER_M mm join BOOK_LEND_RECORD blr on blr.KEEPER_ID = mm.USER_ID
+group by blr.KEEPER_ID , mm.USER_CNAME, mm.USER_ENAME, year(blr.LEND_DATE)
+order by KeeperId , BorrowYear;
+
