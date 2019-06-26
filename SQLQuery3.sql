63902c0f2366d90a59540a3a941a656af7a9afb7
kmg31815 <kmg31815@gmail.com>
2019-06-26 14:41:32 +0800

    homework1-03

---
+++ SQLQuery3.sql
@@ -0,0 +1,12 @@
+select * from BOOK_LEND_RECORD;
+
+select case (MONTH(LEND_DATE)-1)/3
+	when 0 then '2019/01~2019/03'
+	when 1 then '2019/01~2019/03'
+	when 2 then '2019/01~2019/03'
+	when 3 then '2019/01~2019/03' end Quarter , count(BOOK_ID) Cnt
+from BOOK_LEND_RECORD
+where year(LEND_DATE) = 2019
+group by (MONTH(LEND_DATE)-1)/3
+order by (MONTH(LEND_DATE)-1)/3;
+
